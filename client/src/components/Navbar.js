dd17c3c3ff5d9ea78ee93427305488b91a7cd5fe
mamun-bs <mamun.rashid@bs-23.net>
2019-04-04 18:43:02 +0600

    Install React Client app and Implement Login auth

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Navbar extends Component {
+  render() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link className="nav-link" to="/">Home</Link>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/register">Register</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/login">Login</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+  }
+}
+
+export default Navbar;
